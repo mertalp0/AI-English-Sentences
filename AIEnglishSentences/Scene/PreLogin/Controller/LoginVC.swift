bfafe25b51211b647cf58be3c6d3799ab4b482bb
mertalp <mertalp010@gmail.com>
2024-12-15 00:27:40 +0300

    packages added

---
+++ AIEnglishSentences/Scene/PreLogin/Controller/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  AIEnglishSentences
+//
+//  Created by mert alp on 15.12.2024.
+//
+
+import UIKit
+
+final class LoginVC: UIViewController{
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
+}
